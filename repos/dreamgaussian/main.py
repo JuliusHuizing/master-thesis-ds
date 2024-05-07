53c6b204ec69ea26007229310d19a2c6c6801d6e
Julius <jbhuizing@gmail.com>
2024-05-07 10:40:30 +0200

    try own package.

--- repos/dreamgaussian/main.py
+++ repos/dreamgaussian/main.py
@@ -18,7 +18,7 @@ from mesh import Mesh, safe_normalize
 import argparse
 from omegaconf import OmegaConf
 import os
-
+from sisa3d.visuals import visualizer
 class GUI:
     def __init__(self, opt):
         self.opt = opt  # shared with the trainer's opt to support in-place modification of rendering parameters.
@@ -461,55 +461,6 @@ class GUI:
     def render(self):
        return
    
-    def visualize_gaussian_distribution(self, gaussians, image_size=512):
-        import os
-        import numpy as np
-        import matplotlib.pyplot as plt
-
-        # Directory for saving plots
-        plot_dir = "visualization_plots_with_reasonable_reg"
-        if not os.path.exists(plot_dir):
-            os.makedirs(plot_dir)
-
-        scaling_factors = self.renderer.gaussians.get_scaling.detach().cpu().numpy()  # [N, 3] where N is the number of Gaussians
-        opacities = self.renderer.gaussians.get_opacity.detach().cpu().numpy()  # [N, 1]
-        elongation_ratios = np.max(scaling_factors, axis=1) / np.min(scaling_factors, axis=1)
-    
-        # Plotting Elongation Ratios
-        plt.figure(figsize=(10, 5))
-        plt.hist(elongation_ratios, bins=30, color='skyblue', alpha=0.7)
-        plt.title('Distribution of Elongation Ratios')
-        plt.xlabel('Elongation Ratio (Max scaling factor / Min scaling factor)')
-        plt.ylabel('Frequency')
-        plt.grid(True)
-        plot_path = os.path.join(plot_dir, 'elongation_ratios_histogram.png')
-        plt.savefig(plot_path)  # Save the figure
-        plt.close()  # Close the plot to free up memory
-        print(f"Elongation ratios histogram saved to {plot_path}")
-        
-        
-        # Plotting Opacity Distributions
-        plt.figure(figsize=(10, 5))
-        plt.hist(opacities, bins=30, color='salmon', alpha=0.7)
-        plt.title('Distribution of Opacity Values')
-        plt.xlabel('Opacity')
-        plt.ylabel('Frequency')
-        plt.grid(True)
-        plt.savefig(os.path.join(plot_dir, 'opacity_values_histogram.png'))  # Save the figure
-        plt.close()  # Close the plot
-        
-        
-        # Assuming 'scaling_factors' is a tensor from your model
-        variance = torch.var(scaling_factors, dim=1).detach().cpu().numpy()
-        plt.figure(figsize=(10, 6))
-        plt.hist(variance, bins=30, alpha=0.7, color='blue')
-        plt.title('Distribution of Variance among Scaling Factors')
-        plt.xlabel('Variance')
-        plt.ylabel('Frequency')
-        plt.grid(True)
-        plt.savefig(os.path.join(plot_dir, 'variance_distribution_histogram.png'))  # Save the figure
-        plt.show()
-        
     
     # no gui mode
     def train(self, iters=1000):
@@ -518,7 +469,7 @@ class GUI:
             for i in tqdm.trange(iters):
                 self.train_step()
             # visualize gaussian distribution
-            self.visualize_gaussian_distribution(self.renderer.gaussians) 
+            visualizer.visualize_gaussian_distribution(self.renderer.gaussians) 
             # do a last prune
             self.renderer.gaussians.prune(min_opacity=0.01, extent=1, max_screen_size=1)
         # save
--- utils/yaml_utils.py
+++
@@ -1,8 +0,0 @@
-import yaml
-import importlib
-import os
-import yaml
-
-def load_yaml_file(file_path):
-    with open(file_path, 'r') as file:
-        return yaml.safe_load(file)
