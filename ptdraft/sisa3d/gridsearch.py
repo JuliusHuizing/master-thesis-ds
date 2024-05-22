22c7cfaad3b117f995724b5d148cbbcebaf759e1
Julius <jbhuizing@gmail.com>
2024-05-22 14:04:49 +0200

    add image path to grid serach config.

--- ptdraft/sisa3d/gridsearch.py
+++ ptdraft/sisa3d/gridsearch.py
@@ -79,7 +79,19 @@ def create_grid_search_config_files(path_to_default_config_yaml, path_to_grid_se
     # Load the grid search configuration file
     with open(path_to_grid_search_yaml, 'r') as file:
         grid_search_config = yaml.safe_load(file)
+        
+        
+    image_dir = grid_search_config['image_dir']
+    # List to store the paths of all .png files
+    image_paths = []
+    # Walk through the directory
+    for root, dirs, files in os.walk(image_dir):
+        for file in files:
+            if file.endswith('.png'):
+                # Construct the full path and add it to the list
+                image_paths.append(os.path.join(root, file))
 
+  
     # Extract hyperparameters and their values from the grid search config
     hyperparameters = grid_search_config['hyperparameters']
     
@@ -95,22 +107,24 @@ def create_grid_search_config_files(path_to_default_config_yaml, path_to_grid_se
         os.makedirs(output_dir)
     
     # Generate a config file for each combination
-    for idx, combo in enumerate(combinations):
-        # Create a new config based on the default config
-        new_config = yaml.safe_load(yaml.dump(default_config))  # Deep copy
-        
-        # Unflatten the combination to get nested structure
-        nested_combo = unflatten_dict(combo)
-        
-        # Update the config with the current combination of hyperparameters
-        new_config = deep_update_dict(new_config, nested_combo)
-        
-        # Define the output path
-        output_path = os.path.join(output_dir, f'temporary_{idx}.config.yaml')
-        
-        # Write the new config to a file
-        with open(output_path, 'w') as file:
-            yaml.dump(new_config, file)
+    for image_idx, image_path in enumerate(image_paths):
+        for idx, combo in enumerate(combinations):
+            # Create a new config based on the default config
+            new_config = yaml.safe_load(yaml.dump(default_config))  # Deep copy
+            
+            # Unflatten the combination to get nested structure
+            nested_combo = unflatten_dict(combo)
+            
+            # Update the config with the current combination of hyperparameters
+            new_config = deep_update_dict(new_config, nested_combo)
+            
+            new_config['paths']['input_image_path'] = image_path
+            
+            # Define the output path
+            output_path = os.path.join(output_dir, f'temporary_{idx}_for_img_{image_idx}.config.yaml')
+            
+            with open(output_path, 'w') as file:
+                yaml.dump(new_config, file)
     
-    print(f'Generated {len(combinations)} configuration files in {output_dir}.')
+    print(f'Generated {len(combinations)} configuration files in {output_dir} for {len(image_paths)} images.')
 
