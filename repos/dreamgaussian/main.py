95258c870e849df07caf3259db9c4f65118c1056
Julius <jbhuizing@gmail.com>
2024-05-27 14:36:25 +0200

    fml

--- repos/dreamgaussian/main.py
+++ repos/dreamgaussian/main.py
@@ -463,7 +463,7 @@ class GUI:
             capture_and_save_images_for_clip_similarity(self.opt.input, camera_positions, self.opt.stage_1_images_output_path, self.step, self.opt.ref_size, self.cam.fovy, self.cam.fovx, self.cam.near, self.cam.far, self.renderer, orbit_camera, MiniCam)
             # capture_and_save_images(camera_positions, self.opt.stage_1_images_output_path, self.step, self.opt.ref_size, self.cam.fovy, self.cam.fovx, self.cam.near, self.cam.far, self.renderer, orbit_camera, MiniCam)
         if self.opt.stage_1_result_images_output_path is not None:
-            print("saving images for comparison")
+            print("saving images for comparison", flush=True)
             # save_dir = "image_progressions"
             camera_positions = [
                 (0, 0, 2),  # Elevation, Horizontal Angle, Radius
@@ -471,7 +471,8 @@ class GUI:
                 (0, 180, 2)   
             ]
             capture_and_save_images(self.opt.input_image_path, camera_positions, self.opt.stage_1_result_images_output_path, self.step, self.opt.ref_size, self.cam.fovy, self.cam.fovx, self.cam.near, self.cam.far, self.renderer, orbit_camera, MiniCam)
-
+        else:
+            print("No output directory specified for saving images.", flush=True)
 
           
             
