75b366141ab31266f1f6f1a05466fc71cc080c4d
Julius <jbhuizing@gmail.com>
2024-06-14 13:07:51 +0200

    add notes.

--- repos/dreamgaussian/main.py
+++ repos/dreamgaussian/main.py
@@ -96,7 +96,7 @@ class GUI:
         camera_positions = []
         # for 100 horizontal angles between -45 and 45
         for horizontal_angle in np.linspace(0, 360, 10):
-            for elevation in np.linspace(0, 45, 10):
+            for elevation in np.linspace(0, 45, 10): #TODO: widen range, but note taht -90 and 90 degrees seem to lead to very strange result...
                 for radius in np.linspace(1, 3, 10):
                     camera_positions.append((elevation, horizontal_angle, radius))
         
